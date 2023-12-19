1b1dcccecb0a69de8bb1ded0f123f3bc8d96ea50
flavimellow <102172576+flavimellow@users.noreply.github.com>
2023-12-19 15:09:10 -0300

    tentando corrigir menu

--- wp-content/themes/template_inicio/footer.php
+++ wp-content/themes/template_inicio/footer.php
@@ -28,7 +28,31 @@
             <span><a href= "#" class="link-footer">obs: Revisar links quando for pro ar</a></span><br/>
 
         </div>
-
+        <div class="menu2">
+                <nav class="navbar navbar-expand-md navbar-light bg-transparent" role="navigation">
+                    <div class="container">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1"
+                            aria-expanded="false"
+                            aria-label="<?php esc_attr_e( 'Toggle navigation', 'template_inicio' ); ?>">
+                            <span class="navbar-toggler-icon text-end"></span>
+                        </button>
+                        <?php
+        wp_nav_menu( array(
+            'theme_location'    => 'flaviene_lab_main_menu2',
+            'depth'             => 7,
+            'container'         => 'div',
+            'container_class'   => 'collapse navbar-collapse',
+            'container_id'      => 'bs-example-navbar-collapse-1',
+            'menu_class'        => 'navbar-nav',
+            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+            'walker'            => new WP_Bootstrap_Navwalker(),
+        ) );
+        ?>
+                    </div>
+                </nav>
+            </div>
         <div class="child_container_footer politicas">
             <span><a href="#" class="link-footer">Politica de Privacidade</a></span><br/>
             <span><a href="#" class="link-footer">Politica e Uso</a></span><br/>
